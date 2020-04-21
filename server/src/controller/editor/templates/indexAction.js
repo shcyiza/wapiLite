6701dec87bbe06b11ba9c0cab475665fd9b33a07
shcyiza <patick.cyiza@gmail.com>
2020-04-21 22:32:59 +0200

    use node fs lingo

--- server/src/controller/editor/templates/indexAction.js
+++ server/src/controller/editor/templates/indexAction.js
@@ -4,11 +4,12 @@ module.exports = base_dir => (req, res) => {
   try {
     const templates = [];
 
-    const raw_data = fs.readdirSync(base_dir, { withFileTypes: true });
+    const raw_data = fs.readdirSync(base_dir, { withFileTypes: true, withFileStats: true});
 
-    for (const element of raw_data) {
-      if(element.isDirectory()) {
-        const {name} = element
+    for (const dirent of raw_data) {
+      if(dirent.isDirectory()) {
+        const {name} = dirent
+        console.log(dirent)
         const {birthtime, mtime, atime} = fs.statSync(base_dir + name, {}, ()=>{})
 
         templates.push({
